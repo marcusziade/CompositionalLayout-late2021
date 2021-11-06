c6ece5f79ca8b1e6d9719e515eaf9810615752e7
Marcus Ziadé <marcus.ziade@ouraring.com>
2021-11-06 08:55:36 +0200

    Initial Commit

---
+++ CompositionalLayout-late2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayout-late2021
+//
+//  Created by Marcus Ziadé on 6.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
